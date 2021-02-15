4664d6882123931a879dbb103e95e7c2fb488fc0
PerfectELK <PerfectELK@mail.ru>
2021-02-15 08:01:57 +0300

    Начал призму

--- projects/prizma/src/js/index.js
+++ projects/prizma/src/js/index.js
@@ -1,1 +1,3 @@
 import '../scss/index.scss';
+import imgHeader from '../images/header.png';
+import sectionBg from '../images/top-section-bg.png';
--- webpack.config.js
+++ webpack.config.js
@@ -18,62 +18,86 @@ if (!project) {
 const PAGES_DIR = `./projects/${project}/src/pug/pages/`;
 const PAGES = fs.readdirSync(PAGES_DIR).filter(fileName => fileName.endsWith('.pug'));
 
-module.exports = {
-	entry: `./projects/${project}/src/js/index.js`,
-	watch: true,
-	mode: 'development',
-	output: {
-		path: path.resolve(__dirname, 'projects', project, 'dist'),
-		filename: 'js/bundle.min.js',
-	},
-	devServer: {
-		historyApiFallback: true,
-        contentBase: path.resolve(__dirname, 'projects', project, 'dist'),
-        open: true,
-        compress: true,
-        hot: true,
-        port: 8080,
-	},
-	module: {
-		rules: [
-			{
-				test: /\.js$/,
-				exclude: /node_modules/,
-				use: {
-					loader: 'babel-loader',
-					options: {
-						presets: ['@babel/preset-env']
+module.exports = (env, argv) => {
+	
+	let mode = 'development';
+	let watch = true;
+	if (argv.mode === 'production') {
+		mode = 'production';
+		watch = false;
+	}
+	
+	return {
+		entry: `./projects/${project}/src/js/index.js`,
+		watch: watch,
+		mode: mode,
+		output: {
+			path: path.resolve(__dirname, 'projects', project, 'dist'),
+			filename: 'js/bundle.min.js',
+		},
+		devServer: {
+			historyApiFallback: true,
+			contentBase: path.join(__dirname, 'projects', project, 'dist'),
+			open: true,
+			compress: true,
+			liveReload: true,
+			hotOnly: false,
+			hot: false,
+			port: 8080,
+		},
+		module: {
+			rules: [
+				{
+					test: /\.(png|jpe?g|gif)$/i,
+					use: [
+					{
+						loader: 'file-loader',
+						options: {
+							name: path.join('images', '[name].[ext]') ,
+						},
+					},
+					],
+				},
+				{
+					test: /\.js$/,
+					exclude: /node_modules/,
+					use: {
+						loader: 'babel-loader',
+						options: {
+							presets: ['@babel/preset-env']
+						}
 					}
+				},
+				{
+					test: /\.pug$/,
+					loader: 'pug-loader'
+				},
+				{
+					test: /\.s[ac]ss$/i,
+					use: [
+						MiniCssExtractPlugin.loader,
+						'css-loader?url=false',
+						'sass-loader',
+					],
 				}
-			},
-			{
-				test: /\.pug$/,
-				loader: 'pug-loader'
-			},
-			{
-				test: /\.s[ac]ss$/i,
-				use: [
-					MiniCssExtractPlugin.loader,
-					'css-loader',
-					'sass-loader',
-				],
-			}
+			]
+		},
+		optimization: {
+			minimize: true,
+			minimizer: [new TerserJSPlugin({}), new OptimizeCSSAssetsPlugin({})],
+		},
+		plugins: [
+			// new LiveReloadPlugin({
+			// 	hostname: 'localhost',
+			// }),
+			new MiniCssExtractPlugin({
+				filename: 'css/style.css',
+			}),
+			...PAGES.map(page => new HtmlWebpackPlugin({
+				template: `${PAGES_DIR}/${page}`,
+				filename: path.resolve(__dirname, 'projects', project, 'dist', `${page.replace(/\.pug/, '.html')}`),
+			}))
 		]
-	},
-	optimization: {
-		minimize: true,
-		minimizer: [new TerserJSPlugin({}), new OptimizeCSSAssetsPlugin({})],
-	},
-	plugins: [
-		// new LiveReloadPlugin({
-		// 	hostname: 'localhost',
-		// }),
-		new MiniCssExtractPlugin({
-			filename: 'css/style.css',
-		}),
-		...PAGES.map(page => new HtmlWebpackPlugin({
-			template: `${PAGES_DIR}/${page}`,
-			filename: path.resolve(__dirname, 'projects', project, 'dist', `${page.replace(/\.pug/, '.html')}`),
-		}))
-	]
+	}
+	
 };
